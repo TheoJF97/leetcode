2fa2c4524e8a55b47dd34088064bfb7a6b002b06
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2024-01-30 15:38:41 -0500

    findMissingInteger 1-30-2024

--- index.js
+++ index.js
@@ -18,4 +18,34 @@
         // Test case 2
             const counter2 = createCounter(-2);
             console.log([counter2(), counter2(), counter2(), counter2(), counter2()]); // Output: [-2, -1, 0, 1, 2]
-        
+
+// ================== findMissingInteger ======================
+
+// Question: 
+// Write a function that accepts an array of unique integers between 0 and 9 (inclusive),
+// and returns the missing element.
+// The array will always include 9 integers, 
+// with one integer between the numbers of 0 and 9 (inclusive) missing.
+
+// ============================================================
+
+const imperfectArr = [1, 4, 2, 3, 0, 5, 6, 8, 9];
+
+const findMissingInt = (array) => {
+  const perfectArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  // Sort array by increasing order
+  array.sort();
+
+  // Loop through and compare perfectArr to imperfectArr
+  for (let i = 0; i < perfectArr.length; i++) {
+    if (perfectArr[i] !== array[i]) {
+      return perfectArr[i];
+    } 
+  }
+}
+
+// Expected output: 7
+console.log(findMissingInt(imperfectArr));
+
+
