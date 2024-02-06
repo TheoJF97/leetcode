cdd3cd4991e3c931e62c3ca043240542449dfbc0
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2024-02-06 16:21:35 -0500

    Morsecode

--- index.js
+++ index.js
@@ -194,3 +194,85 @@ console.log("LEFT:", loopArray(loopArray1, 'left', 2))
 
 console.log("RIGHT:", loopArray(loopArray1, 'right', 2))
 // should produce result: [8, 7, 1, 5, 87, 45]
+
+// ================== morseCode ======================
+
+// Question: 
+// The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital letters are used. When the message is written in Morse code, a single space is used to separate the character codes and 3 spaces are used to separate words. 
+// Using the dictionary provided, write a function to decode Morse code string. 
+// Decode the following: '.... . -.--   .--- ..- -.. .'
+
+// Morse Code Dictionary:
+const MORSE_CODE = { 
+  '-.-.--': '!',
+  '.-..-.': '"',
+  '...-..-': '$',
+  '.-...': '&',
+  '.----.': '\'',
+  '-.--.': '(',
+  '-.--.-': ')',
+  '.-.-.': '+',
+  '--..--': ',',
+  '-....-': '-',
+  '.-.-.-': '.',
+  '-..-.': '/',
+  '-----': '0',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '---...': ':',
+  '-.-.-.': ';',
+  '-...-': '=',
+  '..--..': '?',
+  '.--.-.': '@',
+  '.-': 'A',
+  '-...': 'B',
+  '-.-.': 'C',
+  '-..': 'D',
+  '.': 'E',
+  '..-.': 'F',
+  '--.': 'G',
+  '....': 'H',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '--.-': 'Q',
+  '.-.': 'R',
+  '...': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '...-': 'V',
+  '.--': 'W',
+  '-..-': 'X',
+  '-.--': 'Y',
+  '--..': 'Z',
+  '..--.-': '_'
+}
+
+// ===================================================
+
+// Solution: 
+function decodeMorse(morseCode) {
+  let sentence = morseCode.split('   ');
+  let newSentence = sentence.map(word => {
+      word = word.split(' ');
+      let newWord = word.map(item => {
+          return MORSE_CODE[item];
+      })
+      return newWord.join('')
+  })
+  return newSentence.join(' ')
+}
+
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
