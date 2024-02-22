316318fc3e1199b1c274dd333a0683f91a67c9d5
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2024-02-22 16:01:13 -0500

    supersecretPW

---
+++ superSecretPW.js
@@ -0,0 +1,20 @@
+// // ================== superSecretPassword ======================
+
+// // Question:
+// // System Admins are always on your case to improve the strength of your passwords. You could really use a handy function to make your passwords completely un-hackable.
+
+// //Use the super secret characters in the superSecretChars variable to replace the matching characters in your totally insecure password and make it un-hackable.
+
+// // eg. replace all 'a's with '@'s. Make sure you get the upper case characters too just in case the user wants to SHOUT their password at you.
+const superSecretChars = [['a', '@'],['s', '$'],['o', '0'], ['h', '5'], ['x', '*']];
+
+// ================================================================
+
+
+function superSecretPasswordRA(password) {
+    superSecretChars.forEach(char => {
+        password = password.replaceAll(char[0], char[1])
+        password = password.replaceAll(char[0].toUpperCase(), char[1])
+    })
+    return password
+}
