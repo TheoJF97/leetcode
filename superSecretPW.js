1a0403f5c956cb82ada2fed301bc4992302262c6
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2024-02-23 05:45:20 -0500

    reduce

--- superSecretPW.js
+++ superSecretPW.js
@@ -1,11 +1,11 @@
-// // ================== superSecretPassword ======================
+// ================== superSecretPassword ======================
 
-// // Question:
-// // System Admins are always on your case to improve the strength of your passwords. You could really use a handy function to make your passwords completely un-hackable.
+// Question:
+// System Admins are always on your case to improve the strength of your passwords. You could really use a handy function to make your passwords completely un-hackable.
 
-// //Use the super secret characters in the superSecretChars variable to replace the matching characters in your totally insecure password and make it un-hackable.
+//Use the super secret characters in the superSecretChars variable to replace the matching characters in your totally insecure password and make it un-hackable.
 
-// // eg. replace all 'a's with '@'s. Make sure you get the upper case characters too just in case the user wants to SHOUT their password at you.
+// eg. replace all 'a's with '@'s. Make sure you get the upper case characters too just in case the user wants to SHOUT their password at you.
 const superSecretChars = [['a', '@'],['s', '$'],['o', '0'], ['h', '5'], ['x', '*']];
 
 // ================================================================
