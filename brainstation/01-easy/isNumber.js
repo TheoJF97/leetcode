fd749cb096a3c61a8816ebbe86aa2a1c716fd1ad
Theophilus Firtandi <theophilus.josiah@hotmail.com>
2024-02-28 10:54:34 -0500

    isNumberFL

---
+++ brainstation/01-easy/isNumber.js
@@ -0,0 +1,38 @@
+/* ================== isNumber =====================
+Question: Write a function that checks if all items in an array are numbers. Return a boolean. 
+
+Input: [0, 1, 4, 6, 8]
+Output: true
+
+Input: [0, "yes", 0]
+Output: false
+
+for loop, linear search on array if all items are numbers 
+================================================= */
+
+const arr1 = [0, 1, 4, 6, 8];
+const arr2 = [0, "yes", 0];
+
+// Using for loop
+const isNumberFL = (arr) => {
+  let result = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== "number") {
+      result = false;
+    }
+  }
+
+  return result;
+};
+
+console.log(isNumberFL(arr1));
+console.log(isNumberFL(arr2));
+
+// Using forEach
+
+// function isNumberFE() = {
+
+// } 
+
+
