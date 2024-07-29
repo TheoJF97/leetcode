174913733d8818bccc6ac148bf29c47969a42782
Theo Firtandi <theophilus.josiah@hotmail.com>
2024-07-29 14:46:07 -0400

    itemInCommonObj

--- leetcode/reverseLinkedList.js
+++ leetcode/reverseLinkedList.js
@@ -13,6 +13,25 @@ Output: [2,1]
 Example 3:
 Input: head = []
 Output: []
+
+APPROACH:
+1. Initialize 3 pointers: previous, current, next node
+2. Iterate through the linked list 
+3. Inside loop, updated nextNode to store next node of curr
+4. Update the next pointer of curr to point to prev 
+5. Move prev to curr, and curr to nextNode 
+6. Continue iterating until curr becomes NULL 
+7. Return prev, which is the new head of the reversed list
+
+DRY RUN 1ST EXAMPLE: head = [1,2,3,4,5]
+Initially: prev = NULL, curr = head, nextNode = NULL.
+Loop 1: nextNode = 2, curr->next = NULL, prev = 1, curr = 2.
+Loop 2: nextNode = 3, curr->next = 1, prev = 2, curr = 3.
+Loop 3: nextNode = 4, curr->next = 2, prev = 3, curr = 4.
+Loop 4: nextNode = 5, curr->next = 3, prev = 4, curr = 5.
+Loop 5: nextNode = NULL, curr->next = 4, prev = 5, curr = NULL.
+Return prev as the new head: [5,4,3,2,1].
+The linked list has been successfully reversed.
 */
 
 const test1 = [1, 2, 3, 4, 5];
@@ -22,11 +41,13 @@ const test3 = [];
 const reverseLinkedList = (head) => {
   let prev = null;
   let curr = head;
+  let nextNode = null;
+
   while (curr != null) {
-    let nextTemp = curr.next;
+    nextNode = curr.next;
     curr.next = prev;
     prev = curr;
-    curr = nextTemp;
+    curr = nextNode;
   }
 
   return prev;
--- other/itemInCommon.js
+++ other/itemInCommon.js
@@ -3,24 +3,77 @@ Question: Compare 2 arrays that have integers; If both arrays have a common elem
 
 test 1 - expected output: true 
 let array1 = [1, 3, 5]
-let array2 = [2, 4 , 5]
+let array2 = [2, 4, 5]
+
+test 1 - expected output: false 
+let array3 = [1, 3, 5]
+let array4 = [2, 4, 6]
 */
 
+/* PSEUDO
+For each element in the array, run another for loop to check if the first element of arr1 equals an element in arr2 - if so return true, else false
+*/
 let array1 = [1, 3, 5];
 let array2 = [2, 4, 5];
-let array3 = [2, 4, 6];
+let array3 = [1, 3, 5];
+let array4 = [2, 4, 6];
+let array5 = [1, 2, 3, 4];
+let array6 = [4, 5, 6];
 
-function itemInCommon(arr1, arr2) {
+const itemInCommon = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// Expected output: true
+// console.log(itemInCommon(array1, array2));
+
+// Expected output: false
+// console.log(itemInCommon(array3, array4));
+
+// Using an obj is a more efficient lookup to keep track of the items in the 1st array
+function itemInCommonObj(arr1, arr2) {
   let obj = {};
   for (let i = 0; i < arr1.length; i++) {
-    // This sets the key of obj as each arr1 element; and the value of each key to true
+    /*
+    Doing the below creates a hash table where 
+    each key is an item from arr1
+    and the value of each key is true
+
+    obj = {
+    1: true
+    3: true
+    5: true
+    }
+    */
+
     obj[arr1[i]] = true;
+    console.log(obj);
   }
+
   for (let j = 0; j < arr2.length; j++) {
+    console.log(obj[arr2[j]]);
     if (obj[arr2[j]]) return true;
   }
   return false;
 }
 
-console.log(itemInCommon(array1, array2));
-console.log(itemInCommon(array1, array3));
+// Expected output: true
+console.log(itemInCommonObj(array1, array2));
+
+// Expected output: false
+console.log(itemInCommonObj(array3, array4));
+
+// Expected output: true
+console.log(itemInCommonObj(array5, array6));
+
+/* Why use an object?
+1. Efficiency: Allows checking if an item exists in arr1 in constant time O(1) vs. linear time O(n)
+2. Simplicity: Code is simple and straightforward, leveraging properties of objects for quick lookups 
+*/
